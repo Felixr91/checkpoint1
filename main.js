23204b2d368dd0e1988416fbd671b19f39487ca0
Felix <felixr91@gmail.com>
2018-11-04 19:20:31 -0700

    edit2

--- main.js
+++ main.js
@@ -1,31 +1,34 @@
-var vilhealth = 100;
-var gokuhealth = 100;
-var gokuhitcount = 0;
-var vilhitcount = 0;
-var villiantransformations = 0;
-var gokutransformations = 0;
-var gokupunch = document.querySelector("#gokupunch");
-var gokukick = document.querySelector("#gokukick");
-var gokublast = document.querySelector("#gokublast");
-var gokuimage = document.querySelector("#goku-image");
-var gokutransform = document.getElementById("gokutransformbtn");
-var vilpunch = document.querySelector("#vilpunch");
-var vilkick = document.querySelector("#vilkick");
-var vilblast = document.querySelector("#vilblast");
-var vilimage = document.querySelector(".vil-image");
-var viltransform = document.getElementById("viltransformbtn");
-var gokudragonball = document.getElementById("gokudragonball");
-var gokusenzubean = document.getElementById("gokusenzubean");
+var goku = {
+  health: 100,
+  attacks: {
+    punch: 10,
+    kick: 15,
+    blast: Math.floor((Math.random() * 25)),
+  },
+  hitcount: 0,
+  powerup: 0,
+  transformations: 0
+}
 
-gokupunch.addEventListener("click", gpunch);
-gokukick.addEventListener("click", gkick);
-gokublast.addEventListener("click", gblast);
-gokutransform.addEventListener("click", gtransform);
+var villian = {
+  health: 100,
+  attacks: {
+    punch: 10,
+    kick: 15,
+    blast: Math.floor((Math.random() * 25)),
+  },
+  hitcount: 0,
+  powerup: 0,
+  transformations: 0
+}
 
-vilpunch.addEventListener("click", vpunch);
-vilkick.addEventListener("click", vkick);
-vilblast.addEventListener("click", vblast);
-viltransform.addEventListener("click", vtransform);
+var modify = {
+  attacks: {
+    punch: 5,
+    kick: 5,
+    blast: Math.floor((Math.random() * 30)),
+  }
+}
 
 window.addEventListener("keydown", checkKeyPressed, false);
 
@@ -46,174 +49,238 @@ function checkKeyPressed(evt) {
     return vblast();
   } else if (evt.which == "73") {
     return vtransform();
-  } else if (evt.which == "69") {
-    return gokuwish();
+  } else if (evt.which == "90") {
+    gokuwish();
+  } else if (evt.which == "78") {
+    return vilwish();
+  } else if (evt.which == "88") {
+    return gokusenzu();
+  } else if (evt.which == "77") {
+    return vilsenzu();
+  } else if (evt.which == "67") {
+    return gokucapsule();
+  } else if (evt.which == "188") {
+    return villiancapsule();
   }
 }
 
 function gpunch() {
-  vilhealth -= 10;
+  villian.health -= goku.attacks.punch;
+  goku.hitcount += 1;
   vilprogressbar();
-  gokuhitcount += 1;
 }
 
 function vpunch() {
-  gokuhealth -= 10;
+  goku.health -= villian.attacks.punch;
+  villian.hitcount += 1;
   gokuprogressbar();
-  vilhitcount += 1;
 }
 
 function gkick() {
-  vilhealth -= 20;
+  villian.health -= goku.attacks.kick;
+  goku.hitcoutn += 1;
   vilprogressbar();
-  gokuhitcount += 1;
 }
 
 function vkick() {
-  gokuhealth -= 20;
+  goku.health -= villian.attacks.kick;
+  villian.hitcount += 1;
   gokuprogressbar();
-  vilhitcount += 1;
 }
 
-
 function gblast() {
-  vilhealth -= Math.floor((Math.random() * 25));
+  villian.health -= goku.attacks.blast;
+  goku.hitcount += 1;
   vilprogressbar();
-  gokuhitcount += 1;
 }
 
 function vblast() {
-  gokuhealth -= Math.floor((Math.random() * 25));
+  goku.health -= Math.floor((Math.random() * 25));
+  villian.hitcount += 1;
   gokuprogressbar();
-  vilhitcount += 1;
 }
 
-function vilprogressbar() {
-  document.getElementById("vilprogress").innerHTML = `
-  <div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" role="progressbar"
-    style="width: ${vilhealth}%" aria-valuenow="${vilhealth}%" aria-valuemin="0" aria-valuemax="100"></div>`
-  checkpowerupvil();
+function gokuwish() {
+  villian.health -= 30;
+  vilprogressbar();
+  document.getElementById("gokudragonball").innerHTML = `
+    `
+}
+
+function vilwish() {
+  goku.health -= 30;
+  gokuprogressbar();
+  document.getElementById("vildragonball").innerHTML = `
+  `
 }
 
 function gokuprogressbar() {
   document.getElementById("gokuprogress").innerHTML = `
   <div class="gokuprogress progress-bar progress-bar-striped progress-bar-animated bg-danger" role="progressbar"
-  style="width: ${gokuhealth}%" aria-valuenow="${gokuhealth}" aria-valuemin="0" aria-valuemax="100"></div>`
+  style="width: ${goku.health}%" aria-valuenow="${goku.health}" aria-valuemin="0" aria-valuemax="100"></div>`
   checkpowerupgoku();
-}
-/*
-gokucheckdead();
-function gokucheckdead() {
-  if (gokuhealth = 0) {
-    alert("goku died");
-  }
-}
-*/
-/*
-function gokuwish() {
-  gokuhealth += 50;
-  document.getElementById("gokuprogress").innerHTML = `
-  <div class="gokuprogress progress-bar progress-bar-striped progress-bar-animated bg-${"info"} role="progressbar"
-  style="width: ${gokuhealth}%" aria-valuenow="${gokuhealth}" aria-valuemin="0" aria-valuemax="100"></div>`
-}
-*/
-/*
-function gokuballappear() {
-  if (gokutransformations = 3) {
-    document.getElementById("gokudragonball").innerHTML = `
-    <img class="dragonball-sizing" src="images/dragonball.png" alt="">
-    `
-  }
-}
-*/
-function gokusenzu() {
-  gokuhealth = 100;
-  document.getElementById("gokuprogress").innerHTML = `
-  <div class="gokuprogress progress-bar progress-bar-striped progress-bar-animated bg-${"info"} role="progressbar"
-  style="width: ${gokuhealth}%" aria-valuenow="${gokuhealth}" aria-valuemin="0" aria-valuemax="100"></div>`
-}
-
-function checkpowerupvil() {
-  if (vilhealth <= 40) {
-    viltransform.classList.remove("vilbuttonhide");
-  }
+  gokusenzubean();
+  gokucapsuleappear();
 }
 
 function checkpowerupgoku() {
-  if (gokuhealth <= 40) {
-    gokutransform.classList.remove("gokubuttonhide");
+  if (goku.health <= 40) {
+    document.getElementById("gokubuttoncontrol").innerHTML = `
+    <button id="gokutransformbtn" class="btn btn-danger viltransformbtn">TRANSFORM</button>
+    `
   }
 }
 
-function vtransform() {
-  var charimages = ["images/cell.gif", "images/buu.gif", "images/freiza.gif", "images/goldenfreiza.gif", "images/jiren.gif"];
-  var displaycaricon = ["images/cell-icon.png", "images/buu-icon.png", "images/freiza-icon.png", "images/goldenfreiza-icon.png", "images/jiren-icon.png"];
-  var cssdisplay = ["vil-image", "buu-sizing", "freiza-sizing", "goldenfreiza-sizing", "jiren-sizing"]
-
-  viltransformplusone();
-
-  document.getElementById("vimage").innerHTML = `
-    <img class=${cssdisplay[villiantransformations]} src=${charimages[villiantransformations]} alt="">
+function gokucapsuleappear() {
+  if (goku.health < 50 && villian.health < 50) {
+    document.getElementById("gokucapsule").innerHTML = `
+    <img class="item-sizing" src="images/capsule.png" alt=""></img>
     `
-  document.getElementById("vil-icon").innerHTML = `
-    <img class="vil-icon" src=${displaycaricon[villiantransformations]} alt="">
+  }
+}
+
+function gokucapsule() {
+  villian.health -= 50;
+  vilprogressbar();
+  document.getElementById("gokucapsule").innerHTML = `
+  `
+}
+
+function vilprogressbar() {
+  document.getElementById("vilprogress").innerHTML = `
+  <div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" role="progressbar"
+    style="width: ${villian.health}%" aria-valuenow="${villian.health}%" aria-valuemin="0" aria-valuemax="100"></div>`
+  checkpowerupvil();
+  villiansenzubean();
+  villiancapsuleappear();
+}
+
+function villiancapsuleappear() {
+  if (goku.health < 50 && villian.health < 50) {
+    document.getElementById("villiancapsule").innerHTML = `
+    <img class="item-sizing" src="images/capsule.png" alt=""></img>
     `
-  vilnametext();
-  vilhealthboost(25);
-  vilhealth += 25;
-  viltransform.classList.add("vilbuttonhide");
+  }
+}
+
+function villiancapsule() {
+  goku.health -= 50;
+  gokuprogressbar();
+  document.getElementById("villiancapsule").innerHTML = `
+  `
+}
+
+function checkpowerupvil() {
+  if (villian.health <= 40) {
+    document.getElementById("vilbuttoncontrol").innerHTML = `
+    <button id="viltransformbtn" class="btn btn-danger viltransformbtn">TRANSFORM</button>
+    `
+  }
 }
 
 function gtransform() {
   var charimages = ["images/goku.gif", "images/gokussj.gif", "images/gokussj2.gif", "images/gokussj3.gif", "images/gokublue.gif"];
   var displaycaricon = ["images/goku-icon.png", "images/gokussj-icon.png", "images/gokussj2-icon.png", "images/gokussj3-icon.png", "images/gokublue-icon.png"];
   var cssdisplay = ["goku-image", "gokussj-sizing", "gokussj2-sizing", "gokussj3-sizing", "gokublue-sizing"];
+  var select = ["Goku", "Super Saiyan Goku", "Super Saiyan 2 Goku", "Super Saiyan 3 Goku", "SSJ God Blue Goku"];
 
   gokutransformplusone();
 
   document.getElementById("gokuimage").innerHTML = `
-  <img class=${cssdisplay[gokutransformations]} src=${charimages[gokutransformations]} alt="">
+  <img class=${cssdisplay[goku.transformations]} src=${charimages[goku.transformations]} alt="">
   `
   document.getElementById("goku-icon").innerHTML = `
-    <img class="goku-icon" src=${displaycaricon[gokutransformations]} alt=""></img>
+    <img class="goku-icon" src=${displaycaricon[goku.transformations]} alt=""></img>
     `
-  gokunametext();
-  gokuhealthboost(25);
-  gokuhealth += 25;
-  gokutransform.classList.add("gokubuttonhide");
-}
+  document.getElementById("gokutextname").innerHTML = `
+    <h1>${select[goku.transformations]}</h1 >
+  `
+  //make button disappear
+  document.getElementById("gokubuttoncontrol").innerHTML = `
 
-function viltransformplusone() {
-  villiantransformations = villiantransformations + 1;
+    `
+  goku.health += 25;
+  gokuprogressbar();
 }
 
 function gokutransformplusone() {
-  gokutransformations = gokutransformations + 1;
+  goku.transformations = goku.transformations + 1;
+  if (goku.transformations === 3) {
+    gokuballappear();
+  }
 }
 
-function vilnametext() {
-  var select = ["Cell", "Majin Buu", "Freiza", "Golden Freiza", "Jiren"];
-  document.getElementById("viltextname").innerHTML = `
-    <h1>${select[villiantransformations]}</h1 >
+function gokuballappear() {
+  document.getElementById("gokudragonball").innerHTML = `
+    <img class="item-sizing" src="images/dragonball.png" alt="">
     `
 }
 
-function gokunametext() {
-  var select = ["Goku", "Super Saiyan Goku", "Super Saiyan 2 Goku", "Super Saiyan 3 Goku", "SSJ God Blue Goku"];
-  document.getElementById("gokutextname").innerHTML = `
-    <h1>${select[gokutransformations]}</h1 >
-  `
+function vtransform() {
+  var charimages = ["images/cell.gif", "images/buu.gif", "images/freiza.gif", "images/goldenfreiza.gif", "images/jiren.gif"];
+  var displaycaricon = ["images/cell-icon.png", "images/buu-icon.png", "images/freiza-icon.png", "images/goldenfreiza-icon.png", "images/jiren-icon.png"];
+  var cssdisplay = ["vil-image", "buu-sizing", "freiza-sizing", "goldenfreiza-sizing", "jiren-sizing"]
+  var select = ["Cell", "Majin Buu", "Freiza", "Golden Freiza", "Jiren"];
+
+  viltransformplusone();
+
+  document.getElementById("vimage").innerHTML = `
+    <img class=${cssdisplay[villian.transformations]} src=${charimages[villian.transformations]} alt="">
+    `
+  document.getElementById("vil-icon").innerHTML = `
+    <img class="vil-icon" src=${displaycaricon[villian.transformations]} alt="">
+    `
+  document.getElementById("viltextname").innerHTML = `
+    <h1>${select[villian.transformations]}</h1 >
+    `
+
+  villian.health += 25;
+  vilprogressbar();
+  //make button disappear
+  document.getElementById("vilbuttoncontrol").innerHTML = `
+
+    `
 }
 
-function vilhealthboost(num) {
-  document.getElementById("vilprogress").innerHTML = `
-  <div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" role="progressbar"
-    style="width: ${vilhealth + num}%" aria-valuenow="${vilhealth + num}%" aria-valuemin="0" aria-valuemax="100"></div>`
+function viltransformplusone() {
+  villian.transformations = villian.transformations + 1;
+  if (villian.transformations === 3) {
+    villianballappear();
+  }
 }
 
-function gokuhealthboost(num) {
-  document.getElementById("gokuprogress").innerHTML = `
-    <div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" role="progressbar"
-    style="width: ${gokuhealth + num}%" aria-valuenow="${gokuhealth + num}%" aria-valuemin="0" aria-valuemax="100"></div>
-  `
+function villiansenzubean() {
+  if (villian.health < 20 && villian.transformations == 4) {
+    document.getElementById("villian-senzubean").innerHTML = `
+      <img class="item-sizing" src="images/senzu-bean.png" alt=""></img>
+      `
+  }
+}
+
+function gokusenzubean() {
+  if (goku.health < 20 && goku.transformations == 4) {
+    document.getElementById("goku-senzubean").innerHTML = `
+      <img class="item-sizing" src="images/senzu-bean.png" alt=""></img>
+      `
+  }
+}
+
+function vilsenzu() {
+  villian.health += 100;
+  vilprogressbar();
+  document.getElementById("villian-senzubean").innerHTML = `
+      `
+}
+
+function gokusenzu() {
+  goku.health += 100;
+  gokuprogressbar();
+  document.getElementById("goku-senzubean").innerHTML = `
+      `
+}
+
+function villianballappear() {
+  document.getElementById("vildragonball").innerHTML = `
+    <img class="item-sizing" src="images/dragonball.png" alt="">
+    `
 }
